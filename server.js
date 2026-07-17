38428b3048bc93ae018565599aa7cce66eb575b5
Muvikri <vito.fikri@gmail.com>
2026-07-17 18:44:04 +0700

    Awal mula perjalanan kita

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { spawn } from 'child_process';
+import path from 'path';
+import fs from "fs";
+import crypto from 'crypto';
+
+const app = express();
+/* 
+ini untuk localhost kalau misal besok udah prod jangan lupa diubah
+jadi pakai http atau https ya.
+*/
+const CFG = { serverUrl: "localhost", port: 6000 };
+
+// pake json
+app.use(express.json());
+// buat endpoint folder
+app.use("/downloads", express.static(path.join(process.cwd(), 'downloads')));
+
+app.post("download", (req, res) => {
+    const { url } = req.body
+    // atur filename sama outputnya
+    const filename = `${crypto.randomUUID()}.mp4`
+    const output = path.join("downloads/", filename);
+
+    const process = spawn("yt-dlp", [
+        "-f", "bv*+ba/b",
+        "--merge-output-format", "mp4",
+        "-o", output,
+        url
+    ]);
+    // logger
+    process.stderr.on("data", (data) => {
+        console.log(data.toString());
+    });
+
+    process.on("close", (code) => {
+        if (code !== 0) {
+            return res.status(500).json({
+                error: "gagal, cek konsol"
+            })
+        }
+    });
+
+    res.json({
+        status: "success",
+        url: `${CFG.serverUrl}:${CFG.port}/downloads/${filename}`
+    });
+});
+app.listen(CFG.port);
